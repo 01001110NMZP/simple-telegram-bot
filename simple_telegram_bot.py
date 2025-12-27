dbd2ddbd2870723a8ba81d09861a2431fe88d92c
Nima Masoumi <159164960+01001110NMZP@users.noreply.github.com>
2025-12-27 20:01:13 +0330

    created "simple_telegram_bot.py"
    + imported python-telegram-bot libraries
    + put the bot token in the code

---
+++ simple_telegram_bot.py
@@ -0,0 +1,9 @@
+from telegram import Update
+from telegram.ext import (Application,
+                          CommandHandler,
+                          MessageHandler,
+                          filters,)
+
+video_tutorial: str = "https://youtu.be/vZtm1wuA2yc?si=iGlFM8LeinbjQ9bS"
+
+TOKEN: str = "8292332768:AAHndDabt6_knDh2GeC4B7hYH3_olsUNjpc"
