447044254d578e954d43eadefc7c6a5ca13be57a
Nima Masoumi <159164960+01001110NMZP@users.noreply.github.com>
2025-12-27 20:08:25 +0330

    Added functions for /start and message handler
    + added /start command functionality
    + added message handling function to echo back user-sent messages back to themselves
    
    - didn't made the application to use the functions yet

--- simple_telegram_bot.py
+++ simple_telegram_bot.py
@@ -4,6 +4,33 @@ from telegram.ext import (Application,
                           MessageHandler,
                           filters,)
 
-video_tutorial: str = "https://youtu.be/vZtm1wuA2yc?si=iGlFM8LeinbjQ9bS"
+# video_tutorial: str = "https://youtu.be/vZtm1wuA2yc?si=iGlFM8LeinbjQ9bS"
 
 TOKEN: str = "8292332768:AAHndDabt6_knDh2GeC4B7hYH3_olsUNjpc"
+
+# Commands
+
+async def start_command(update: Update, context) -> None:
+    """Greets the user"""
+
+    await update.message.reply_text("سلام!\n به بات ما خوش اومدی!")
+
+
+# Response functions
+
+async def message_handler(update: Update, context) -> None:
+    """
+    echos back whatever the users sends
+    and logs who said what and where in the run terminal
+    """
+
+    text: str = update.message.text
+    user_id: int = update.message.chat.id
+
+    print(f"User ({user_id}) said: {text}")
+
+    await update.message.reply_text(text)
+
+async def error(update: Update, context) -> None:
+    """in the run terminal, shows what went wrong"""
+    print(f"Update {update} has caused this error {context.error}")
